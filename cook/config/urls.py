40d7c966715f73d6d27614c1dc4bf9472fc6d4de
Kostya <leap74rus@gmail.com>
2022-01-31 23:20:48 +0300

    add home page home.html

--- cook/config/urls.py
+++ cook/config/urls.py
@@ -9,6 +9,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
     path('', include('blog.urls')),
 ]
 
