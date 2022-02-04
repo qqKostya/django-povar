b9b16799f03402ff66f3a10730e84cf1a99c8f6a
Kostya <leap74rus@gmail.com>
2022-02-04 19:33:14 +0300

    add about and contact

--- cook/contact/urls.py
+++ cook/contact/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('contact/', views.ContactView.as_view(), name="contact"),
+    path('about/', views.AboutView.as_view(), name="about"),
     path('feedback/', views.CreateContact.as_view(), name="feedback"),
 ]
